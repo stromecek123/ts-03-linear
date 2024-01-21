ed32ee7994208051d90af6f313d3bad02f1e7a32
stromecek123 <richard.abraham.023@pslib.cz>
2024-01-21 18:57:29 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 let hodnota1: number = 6;
 let pocetHodnot: number = 44;
 let vetsi: number = 4;
-let kazde: number= 5;
+let kazde: number = 5;
 
 let pocet: number = 0;
 
-while (pocet <= pocetHodnot){
-    hodnota1 += vetsi
-    pocet += 1
+while (pocet < pocetHodnot){
 
     if (pocet % kazde !== 0) {
       console.log(hodnota1)
     }
-}
+    
+  hodnota1 += vetsi
+  pocet += 1
+  }
