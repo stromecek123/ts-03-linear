bad8447c0a36618e561c3c161953fab175cc877f
stromecek123 <richard.abraham.023@pslib.cz>
2024-01-21 18:43:54 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+let hodnota1: number = 6;
+let pocetHodnot: number = 44;
+let vetsi: number = 4;
+let kazde: number= 5;
+
+let pocet: number = 0;
+
+while (pocet <= pocetHodnot){
+    hodnota1 += vetsi
+    pocet += 1
+
+    if (pocet % kazde !== 0) {
+      console.log(hodnota1)
+    }
+}
